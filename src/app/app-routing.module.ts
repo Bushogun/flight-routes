5b65f9e8a0fb50308df22849760505a4a3223ba9
JonathanSoto <61751017+Bushogun@users.noreply.github.com>
2024-01-05 11:41:53 -0500

    feat: :sparkles: setting developer environment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+//   {
+//     path: 'flights',
+//     component: LayoutPageComponent,
+//     //loadChildren: () => import('./flights/pages/layout-page/layout-page.component').then( m => m.FlightModule ),
+//   },
+//   {
+//     path: '404',
+//     component: Error404PageComponent,
+//   },
+//   {
+//     path: '',
+//     redirectTo: 'flights',
+//     pathMatch: 'full'
+//   },
+//   {
+//     path: '**',
+//     redirectTo: '404',
+// }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
